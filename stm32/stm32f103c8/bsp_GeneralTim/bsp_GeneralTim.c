1ac4f1b0f73cfff79d448e59430a1a0599385599
xiaobo <xiaoboliu0602@163.com>
2022-03-30 14:13:02 +0800

    update

--- stm32/stm32f103c8/bsp_GeneralTim/bsp_GeneralTim.c
+++ stm32/stm32f103c8/bsp_GeneralTim/bsp_GeneralTim.c
@@ -1,28 +1,34 @@
-/*********************** 2018_05_18 *********************
- *文件名  ：bsp_GeneralTim.c
- *描述    ：定时器配置
- *硬件连接：不同IO口对于定时器不同
- *库版本  ：ST3.5.0
- *********************************************************/
+/**
+  ******************************************************************************
+  * @file    bsp_GeneralTim.c
+  * @author  Bob
+  * @version V3.5.0
+  * @date    30-March-2022
+  * @brief   Time base configuration
+  ******************************************************************************
+  * @attention
+  *
+  ******************************************************************************
+  */
 
 #include "bsp_GeneralTim.h"
 
-// 定时器输入捕获用户自定义变量结构体定义
+/* Input user value type */
 TIM_ICUserValueTypeDef TIM_ICUserValueStructure = {0,0,0,0};
 
-// 中断优先级配置
+/* Configure the nested vectored interrupt controller. */
 static void GENERAL_TIM_NVIC_Config(void)
 {
-    NVIC_InitTypeDef NVIC_InitStructure; 
-    // 设置中断组0
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);		
-		// 设置中断来源
-    NVIC_InitStructure.NVIC_IRQChannel = GENERAL_TIM_IRQ;	
-		// 设置主优先级为 0
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;	 
-	  // 设置抢占优先级为3
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;	
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+
+    /* Enable the GENERAL_TIM_IRQ global Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = GENERAL_TIM_IRQ;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
     NVIC_Init(&NVIC_InitStructure);
 }
 
@@ -30,30 +36,29 @@ static void GENERAL_TIM_GPIO_Config(void)
 {
   GPIO_InitTypeDef GPIO_InitStructure;
 
-  // 输入捕获通道 GPIO 初始化
-	RCC_APB2PeriphClockCmd(GENERAL_TIM_CH3_GPIO_CLK, ENABLE);
+  RCC_APB2PeriphClockCmd(GENERAL_TIM_CH3_GPIO_CLK, ENABLE);
   GPIO_InitStructure.GPIO_Pin =  GENERAL_TIM_CH3_PIN;
   //GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
-	//GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+  //GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GENERAL_TIM_CH3_PORT, &GPIO_InitStructure);	
 }
 
 
-///*
-// * 注意：TIM_TimeBaseInitTypeDef结构体里面有5个成员，TIM6和TIM7的寄存器里面只有
-// * TIM_Prescaler和TIM_Period，所以使用TIM6和TIM7的时候只需初始化这两个成员即可，
-// * 另外三个成员是通用定时器和高级定时器才有.
-// *-----------------------------------------------------------------------------
-// *typedef struct
-// *{ TIM_Prescaler            都有
-// *	TIM_CounterMode			     TIMx,x[6,7]没有，其他都有
-// *  TIM_Period               都有
-// *  TIM_ClockDivision        TIMx,x[6,7]没有，其他都有
-// *  TIM_RepetitionCounter    TIMx,x[1,8,15,16,17]才有
-// *}TIM_TimeBaseInitTypeDef; 
-// *-----------------------------------------------------------------------------
-// */
+/*
+* 注意：TIM_TimeBaseInitTypeDef结构体里面有5个成员，TIM6和TIM7的寄存器里面只有
+* TIM_Prescaler和TIM_Period，所以使用TIM6和TIM7的时候只需初始化这两个成员即可，
+* 另外三个成员是通用定时器和高级定时器才有.
+*-----------------------------------------------------------------------------
+*typedef struct
+*{ TIM_Prescaler            都有
+*  TIM_CounterMode			     TIMx,x[6,7]没有，其他都有
+*  TIM_Period               都有
+*  TIM_ClockDivision        TIMx,x[6,7]没有，其他都有
+*  TIM_RepetitionCounter    TIMx,x[1,8,15,16,17]才有
+*}TIM_TimeBaseInitTypeDef; 
+*-----------------------------------------------------------------------------
+*/
 
 /* ----------------   PWM信号 周期和占空比的计算--------------- */
 // ARR ：自动重装载寄存器的值
@@ -63,59 +68,48 @@ static void GENERAL_TIM_GPIO_Config(void)
 
 static void GENERAL_TIM_Mode_Config(void)
 {
-	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-	TIM_ICInitTypeDef TIM_ICInitStructure;
-	
-  // 开启定时器时钟,即内部时钟CK_INT=72M
-	GENERAL_TIM_APBxClock_FUN(GENERAL_TIM_CLK,ENABLE);
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+  TIM_ICInitTypeDef TIM_ICInitStructure;
 
-/*--------------------时基结构体初始化-------------------------*/	
-  
-	// 自动重装载寄存器的值，累计TIM_Period+1个频率后产生一个更新或者中断
-	TIM_TimeBaseStructure.TIM_Period=GENERAL_TIM_PERIOD;	
-	// 驱动CNT计数器的时钟 = Fck_int/(psc+1)
-	TIM_TimeBaseStructure.TIM_Prescaler= GENERAL_TIM_PSC;	
-	// 时钟分频因子 ，配置死区时间时需要用到
-	TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;		
-	// 计数器计数模式，设置为向上计数
-	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;		
-	// 重复计数器的值，没用到不用管
-	TIM_TimeBaseStructure.TIM_RepetitionCounter=0;	
-	// 初始化定时器
-	TIM_TimeBaseInit(GENERAL_TIM, &TIM_TimeBaseStructure);
+  GENERAL_TIM_APBxClock_FUN(GENERAL_TIM_CLK,ENABLE);
 
-	/*--------------------输入捕获结构体初始化-------------------*/	
-	
-	// 配置输入捕获的通道，需要根据具体的GPIO来配置
-	TIM_ICInitStructure.TIM_Channel = GENERAL_TIM_CHANNEL_x;
-	// 输入捕获信号的极性配置
-	TIM_ICInitStructure.TIM_ICPolarity = GENERAL_TIM_STRAT_ICPolarity;
-	// 输入通道和捕获通道的映射关系，有直连和非直连两种
-	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
-	// 输入的需要被捕获的信号的分频系数
-	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
-	// 输入的需要被捕获的信号的滤波系数
-	TIM_ICInitStructure.TIM_ICFilter = 0;
-	// 定时器输入捕获初始化
-	TIM_ICInit(GENERAL_TIM, &TIM_ICInitStructure);
+  // 自动重装载寄存器的值，累计TIM_Period+1个频率后产生一个更新或者中断
+  TIM_TimeBaseStructure.TIM_Period=GENERAL_TIM_PERIOD;
+  // 驱动CNT计数器的时钟 = Fck_int/(psc+1)
+  TIM_TimeBaseStructure.TIM_Prescaler= GENERAL_TIM_PSC;
+  TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;
+  TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;
+  TIM_TimeBaseStructure.TIM_RepetitionCounter=0;
+  TIM_TimeBaseInit(GENERAL_TIM, &TIM_TimeBaseStructure);
 
-	
-	// 清除更新和捕获中断标志位
+  /*--------------------输入捕获结构体初始化-------------------*/
+
+  // 配置输入捕获的通道，需要根据具体的GPIO来配置
+  TIM_ICInitStructure.TIM_Channel = GENERAL_TIM_CHANNEL_x;
+  // 输入捕获信号的极性配置
+  TIM_ICInitStructure.TIM_ICPolarity = GENERAL_TIM_STRAT_ICPolarity;
+  // 输入通道和捕获通道的映射关系，有直连和非直连两种
+  TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  // 输入的需要被捕获的信号的分频系数
+  TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+  // 输入的需要被捕获的信号的滤波系数
+  TIM_ICInitStructure.TIM_ICFilter = 0;
+  // 定时器输入捕获初始化
+  TIM_ICInit(GENERAL_TIM, &TIM_ICInitStructure);
+
   TIM_ClearFlag(GENERAL_TIM, TIM_FLAG_Update|GENERAL_TIM_IT_CCx);	
-	
-  // 开启更新和捕获中断  
-	//TIM_ITConfig (GENERAL_TIM, TIM_IT_Update | GENERAL_TIM_IT_CCx, ENABLE );
-	TIM_ITConfig (GENERAL_TIM,  GENERAL_TIM_IT_CCx, ENABLE );
-	
-	// 使能计数器
-	TIM_Cmd(GENERAL_TIM, ENABLE);
+
+  //TIM_ITConfig (GENERAL_TIM, TIM_IT_Update | GENERAL_TIM_IT_CCx, ENABLE );
+  TIM_ITConfig (GENERAL_TIM,  GENERAL_TIM_IT_CCx, ENABLE );
+
+  TIM_Cmd(GENERAL_TIM, ENABLE);
 }
 
 void GENERAL_TIM_Init(void)
 {
-	GENERAL_TIM_GPIO_Config();
-	GENERAL_TIM_NVIC_Config();
-	GENERAL_TIM_Mode_Config();		
+  GENERAL_TIM_GPIO_Config();
+  GENERAL_TIM_NVIC_Config();
+  GENERAL_TIM_Mode_Config();
 }
 
-/*********************************************END OF FILE**********************/
+/*********************************END OF FILE**********************************/
--- stm32/stm32f103c8/bsp_GeneralTim/bsp_GeneralTim.h
+++ stm32/stm32f103c8/bsp_GeneralTim/bsp_GeneralTim.h
@@ -1,9 +1,7 @@
 #ifndef __BSP_GENERALTIME_H
 #define __BSP_GENERALTIME_H
 
-
 #include "stm32f10x.h"
-
 
 /************通用定时器TIM参数定义，只限TIM2、3、4、5************/
 // 当使用不同的定时器的时候，对应的GPIO是不一样的，这点要注意
@@ -13,23 +11,23 @@
 #define            GENERAL_TIM_APBxClock_FUN     RCC_APB1PeriphClockCmd
 #define            GENERAL_TIM_CLK               RCC_APB1Periph_TIM4
 #define            GENERAL_TIM_PERIOD            0XFFFF
-#define            GENERAL_TIM_PSC              (72-1)
+#define            GENERAL_TIM_PSC               (72-1)
 
 // TIM 输入捕获通道GPIO相关宏定义
 #define            GENERAL_TIM_CH3_GPIO_CLK      RCC_APB2Periph_GPIOB
 #define            GENERAL_TIM_CH3_PORT          GPIOB
 #define            GENERAL_TIM_CH3_PIN           GPIO_Pin_8
-#define            GENERAL_TIM_CHANNEL_x         TIM_Channel_3    //注意3
+#define            GENERAL_TIM_CHANNEL_x         TIM_Channel_3    // 3
 
 // 中断相关宏定义
-#define            GENERAL_TIM_IT_CCx            TIM_IT_CC3        //注意3
+#define            GENERAL_TIM_IT_CCx            TIM_IT_CC3        // 3
 #define            GENERAL_TIM_IRQ               TIM4_IRQn
 #define            GENERAL_TIM_INT_FUN           TIM4_IRQHandler
 
 // 获取捕获寄存器值函数宏定义
-#define            GENERAL_TIM_GetCapturex_FUN                 TIM_GetCapture3         //注意3
+#define            GENERAL_TIM_GetCapturex_FUN                 TIM_GetCapture3         // 3
 // 捕获信号极性函数宏定义
-#define            GENERAL_TIM_OCxPolarityConfig_FUN           TIM_OC3PolarityConfig   //注意3
+#define            GENERAL_TIM_OCxPolarityConfig_FUN           TIM_OC3PolarityConfig   // 3
 
 // 测量的起始边沿
 #define            GENERAL_TIM_STRAT_ICPolarity                TIM_ICPolarity_Rising
@@ -40,17 +38,16 @@
 // 定时器输入捕获用户自定义变量结构体声明
 typedef struct
 {   
-	uint8_t   Capture_FinishFlag;   // 捕获结束标志位
-	uint8_t   Capture_StartFlag;    // 捕获开始标志位
-	uint16_t  Capture_CcrValue;     // 捕获寄存器的值
-	uint16_t  Capture_Period;       // 自动重装载寄存器更新标志 
+  uint8_t   Capture_FinishFlag;   // 捕获结束标志位
+  uint8_t   Capture_StartFlag;    // 捕获开始标志位
+  uint16_t  Capture_CcrValue;     // 捕获寄存器的值
+  uint16_t  Capture_Period;       // 自动重装载寄存器更新标志 
 }TIM_ICUserValueTypeDef;
 
 extern TIM_ICUserValueTypeDef TIM_ICUserValueStructure;
 
-/**************************函数声明********************************/
-void GENERAL_TIM_Init(void);
 
+void GENERAL_TIM_Init(void);
 
 #endif	/* __BSP_GENERALTIME_H */
 
